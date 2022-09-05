77dddfbc2f99f62dce5940e91e9dcdcaade2fcc9
mobileapp-max <ma9nez@gmail.com>
2022-09-05 13:54:24 -0400

    sep 5

--- screens/AddQ.tsx
+++ screens/AddQ.tsx
@@ -1,53 +1,115 @@
-import React from "react";
-import { StyleSheet, View, Dimensions, } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View, Dimensions, Modal, Text, Alert, Pressable } from "react-native";
 import TextInput from "./TextInput";
 
 
 const { height, width } = Dimensions.get("window");
 
-
-
 export default function AddQ() {
-
-  console.log('fucking bitch ass laptop')
-
+  const [modalVisible, setModalVisible] = useState(true);
   return (
-    <View style={{ ...styles.container, width }}>
-      <TextInput
-        placeholderTextColor={"black"}
-        placeholder={'bitch ass laptop'}
-        style={styles.input}
-      />
+    <View style={styles.centeredView}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={() => setModalVisible(true)}
+      >
+        <Text style={styles.textStyle}>Show Modal</Text>
+      </Pressable>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  root: {
+  centeredView: {
     flex: 1,
-    margin: 3,
-    // backgroundColor: COLORS[1]
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
   },
-  container: {
-    flex: 1,
-    margin: 16,
-
-    // backgroundColor: COLORS[1]
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
   },
-  content: {
-    height: height * 2
-  },
-  card: {
-    // height: cardTitle,
-    borderRadius: 10,
-    padding: 14,
-
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
+  button: {
+    borderRadius: 20,
     padding: 10,
-    width: width * 0.8,
+    elevation: 2
   },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
 });
+
+//     <>
+//       <View style={{ ...styles.container, width }}>
+//         <TextInput
+//           placeholderTextColor={"blue"}
+//           placeholder={'Title'}
+//           style={styles.input}
+//         />
+//         <TextInput
+//           placeholderTextColor={"blue"}
+//           placeholder={'Question'}
+//           style={styles.input}
+//         />
+//       </View>
+//     </>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     margin: 16,
+//   },
+//   input: {
+//     // height: 40,
+//     // margin: 12,
+//     borderWidth: 1,
+//     padding: 10,
+//     width: width * 0.8,
+//   },
+// });
