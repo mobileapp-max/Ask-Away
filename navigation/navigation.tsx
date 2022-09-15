e442b71efed81fd369973ea40b6e5aba5b6e667a
mobileapp-max <ma9nez@gmail.com>
2022-09-14 22:47:48 -0400

    screen re name

--- navigation/navigation.tsx
+++ navigation/navigation.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import QuestionScreen from "../screens/QuestionScreen";
-import QScreen from "../screens/QScreen";
+import QuestionScreen from "../screens/Question";
+import QScreen from "../screens/ListOfQuestions";
 
 const MainStack = createStackNavigator();
 
