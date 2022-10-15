ec4aa66f942e95ef9b6e7c754b202cbce0868e96
mobileapp-max <ma9nez@gmail.com>
2022-10-15 15:14:41 -0400

    profile screen - name

--- navigation/logInSingUpNav.tsx
+++ navigation/logInSingUpNav.tsx
@@ -3,13 +3,14 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import SignUpScreen from "../screens/SignUpScreen";
 import ProfileScreen from "../screens/ProfileScreen";
+import Profile from "../screens/Profile";
 
 const LogInStack = createStackNavigator();
 
 export const LogInStackScreen = () => (
     <LogInStack.Navigator
         screenOptions={{ headerShown: false }}>
-        <LogInStack.Screen name='Profile' component={ProfileScreen} />
+        <LogInStack.Screen name='Profile' component={Profile} />
         <LogInStack.Screen name="Signup" component={SignUpScreen} />
     </LogInStack.Navigator>
 
