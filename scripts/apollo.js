6d6fb618e9769ee80c9ec8ad276d193827ccdde5
mobileapp-max <ma9nez@gmail.com>
2023-01-23 22:20:36 -0500

    websoccet

--- scripts/apollo.js
+++ scripts/apollo.js
@@ -1,12 +1,21 @@
-import { ApolloClient } from 'apollo-client';
+import { ApolloClient } from '@apollo/client';
+// import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import { InMemoryCache } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+// import { InMemoryCache } from 'apollo-cache-inmemory';
 const GRAPHQL_ENDPOINT = `https://mature-humpback-99.hasura.app/v1/graphql`;
 const createApolloClient = (token) => {
-    const link = new HttpLink({
+    // const link = new HttpLink({
+    const link = new WebSocketLink({
         uri: GRAPHQL_ENDPOINT,
-        headers: {
-            'Authorization': `Bearer ${token}`
+        options: {
+            reconnect: true,
+            connectionParams: {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            }
         }
     });
     return new ApolloClient({
